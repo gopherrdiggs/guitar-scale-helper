3069a51ae712e640a169ea1966383892600e1875
grodgers <grodgers@GR20170626>
2019-03-18 17:58:05 -0500

    Remove images; update manifest and stencil config

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,6 +3,7 @@ import { Config } from '@stencil/core';
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
+  copy: [{ src: '_redirects' }],
   outputTargets: [{ type: 'www' }],
   globalScript: 'src/global/app.ts',
   globalStyle: 'src/global/app.css'
